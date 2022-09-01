a9e8a4ab909511d0a517286b0e8233bfb7ec9c4a
DimaSmaha <103424643+DimaSmaha@users.noreply.github.com>
2022-09-01 23:19:56 +0300

    Revert "played a little"

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,12 +1,10 @@
 // in cypress/support/index.ts
 // load type definitions that come with Cypress module
+/// <reference types="cypress" />
 
-// /// <reference types="cypress" />
+declare namespace Cypress {
+    interface Chainable {
+        typeLogin: (username: string, password:string) => void;
+    }
+}
 
-// declare namespace Cypress {
-//     interface Chainable {
-//         typeLogin: (username: string, password:string) => void;
-//     }
-// }
-
-///I guess no sence to declare custom commands here, better to declare inside of file with command
